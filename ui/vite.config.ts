f21176e939c1c3a7dbeaed9a47f29988d130b3cf
= <tylerherwig@gmail.com>
2025-02-18 16:49:59 -0500

    Revert "PWA updates"
    This reverts commit c8246c5b52a42fc7d9e5fe836f7e0c16b10a8220.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,44 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: 'Listify',
-        short_name: 'Listify',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/Vite.js.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-        screenshots: [
-          {
-              "src": "/screenshots/mobile.png",
-              "sizes": "1080x1920",
-              "type": "image/png",
-              "form_factor": "narrow"
-          },
-          {
-              "src": "/screenshots/desktop.png",
-              "sizes": "1920x1080",
-              "type": "image/png",
-              "form_factor": "wide"
-          }
-      ]
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
